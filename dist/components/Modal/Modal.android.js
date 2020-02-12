464366ef03efabb98156bc65df7d0bc293bcf869
Nguyễn Thế Bách <thebach1802@gmail.com>
2020-02-12 21:40:14 +0700

    itntial

---
+++ dist/components/Modal/Modal.android.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) JOB TODAY S.A. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import React, { useEffect } from "react";
+import { BackHandler, View, StyleSheet, StatusBar } from "react-native";
+const Modal = ({ visible, children, onRequestClose }) => {
+    useEffect(() => {
+        const backHandler = BackHandler.addEventListener("hardwareBackPress", () => {
+            if (typeof onRequestClose === "function") {
+                onRequestClose();
+            }
+            return true;
+        });
+        return () => {
+            backHandler.remove();
+        };
+    }, []);
+    return <>{visible && <View style={styles.root}>{children}</View>}</>;
+};
+const styles = StyleSheet.create({
+    root: {
+        ...StyleSheet.absoluteFillObject,
+        zIndex: 1000,
+        backgroundColor: "transparent",
+        top: StatusBar.currentHeight
+    }
+});
+export default Modal;
